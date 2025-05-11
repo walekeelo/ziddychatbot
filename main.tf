aab37c50d350a9512a3cb12a2a595b9bd6535fab
Abdulazeez Balogun <balogun.moyosore@gmail.com>
2025-05-11 10:31:40 +0000

    fixed sku issue

--- main.tf
+++ main.tf
@@ -1,23 +1,3 @@
-provider "azurerm" {
-  features {}
-}
-
-data "azurerm_resource_group" "existing_rg" {
-  name = "milanRG"
-}
-
-resource "azurerm_cognitive_account" "ziddy_openai" {
-  name                = "ziddyopenaiacct"
-  location            = data.azurerm_resource_group.existing_rg.location
-  resource_group_name = data.azurerm_resource_group.existing_rg.name
-  kind                = "OpenAI"
-  sku_name            = "S0"
-
-  tags = {
-    environment = "dev"
-  }
-}
-
 resource "azurerm_cognitive_deployment" "ziddy_gpt" {
   name                 = "ziddy-gpt-deployment"
   cognitive_account_id = azurerm_cognitive_account.ziddy_openai.id
